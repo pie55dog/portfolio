f917fc9271171201e2945e530aca28e438396198
Grace <grace@usc-secure-wireless-new244.usc.edu>
2026-08-30 23:54:30 -0700

    added threads

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,13 @@ import mag12 from './assets/SeniorMag/mag12.jpg'
 import mag13 from './assets/SeniorMag/mag13.jpg'
 import yearbook1 from './assets/Yearbook/Yearbook_Portfolio.jpg'
 import yearbook2 from './assets/Yearbook/Yearbook_Portfolio2.jpg'
+import threadsCover from './assets/threads/cover.jpg'
+import threadsVertical from './assets/threads/vertical.jpg'
+import threads07 from './assets/threads/objectss.png'
+import threadsSeniors from './assets/threads/seniors.png'
+import threadsOpening from './assets/threads/opening.png'
+import threadsTeams from './assets/threads/teams.png'
+import threadsNormal from './assets/threads/normal.png'
 import canWord from './assets/Sputnik/Can Mockup word.png'
 import canMockup from './assets/Sputnik/Can Mockup.png'
 import packaging from './assets/Sputnik/Packaging Flat Lay.png'
@@ -73,12 +80,29 @@ function MagazineSlideshow() {
 	)
 }
 
+function ThreadsGallery() {
+	return (
+		<div className="threads-work">
+			<img className="threads-cover" src={threadsCover} alt="Yearbook 2026 cover" />
+			<img className="threads-vertical" src={threadsVertical} alt="Yearbook 2026 vertical spread" />
+			<img className="threads-teams" src={threadsTeams} alt="Yearbook 2026 teams spread" />
+			<img className="threads-normal" src={threadsNormal} alt="Yearbook 2026 normal spread" />
+			<img className="threads-07" src={threads07} alt="Yearbook 2026 page 07" />
+			
+			<img className="threads-opening" src={threadsOpening} alt="Yearbook 2026 opening spread" />
+			
+		</div>
+	)
+}
+
 function App() {
 	const projects = [
 		['magazine', '01', 'Senior Magazine'],
-		['yearbook', '02', 'Yearbook 2025'],
-		['sputnik', '03', 'Sputnik Branding'],
+		['yearbook-2026', '02', 'Yearbook 2026'],
+		['yearbook', '03', 'Yearbook 2025'],
+		
 		['type-studies', '04', 'Type Studies'],
+		['sputnik', '05', 'Sputnik Branding'],
 	]
 
 	return (
@@ -94,17 +118,20 @@ function App() {
 			</header>
 
 			<Section id="magazine" number="01" title="Senior Magazine" blurb={'Each year, the seniors in the yearbook class create a magazine honoring the graduating class. I designed, oversaw, and produced the 2026 edition, “Roll Credits.” '}><MagazineSlideshow /></Section>
-			<Section id="yearbook" number="02" title="Yearbook 2025" blurb={'A project overview and style guide from my 11th grade yearbook.\n\nIn junior year, I was one of three Editors-in-Chief and design lead. I collaborated and received feedback on all of the pages you see displayed, but the design and writing were created and finalized by me. The theme (which includes the colors, fonts, vibe, etc) was a team effort, but I was its primary creator and I designed most elements on the style guide, including the folio, motifs, colors, and typography style. I also created the cover.\n\nAll of our yearbook work is done in InDesign. I did not take any of the photos on these pages.'}>
+			<Section id="yearbook-2026" number="02" title="Yearbook 2026" blurb={'blurb here\npart 2 blurb'}>
+				<ThreadsGallery />
+			</Section>
+			<Section id="yearbook" number="03" title="Yearbook 2025" blurb={'A project overview and style guide from my 11th grade yearbook.\n\nIn junior year, I was one of three Editors-in-Chief and design lead. I collaborated and received feedback on all of the pages you see displayed, but the design and writing were created and finalized by me. The theme (which includes the colors, fonts, vibe, etc) was a team effort, but I was its primary creator and I designed most elements on the style guide, including the folio, motifs, colors, and typography style. I also created the cover.\n\nAll of our yearbook work is done in InDesign. I did not take any of the photos on these pages.'}>
 				<div className="yearbook-work"><img src={yearbook1} alt="Yearbook 2025 spread one" /><img src={yearbook2} alt="Yearbook 2025 spread two" /></div>
 			</Section>
-			<Section id="sputnik" number="03" title="Sputnik Branding" blurb={'A passion project exploring branding and packaging design.\n\nSputnik Tennis Balls came from two brief moments of inspiration. I was first inspired by the bold, eye-catching design of vintage tennis ball containers, which feature contrasting typography, fun graphics, and a playfulness that has been lost in modern sports marketing. Completely separately, I was struck by the shape of the Soviet Union’s Sputnik 1 satellite. I used an image of it as a reference when designing the flying tennis ball on the can (the tennis ball serves the satellite body while the white lines serve as the satellite legs), which I think brings the whole concept together.\n\nI created the vector art in Adobe Illustrator, and I created the mockup in Womp 3D and Adobe Photoshop. '}>
+			<Section id="type-studies" number="04" title="Type Studies" blurb={'Various project with typography.\n\nIn my spare time, I like playing around with type by making posters. The first three are brief type studies, and the “Brooke” poser is a typographic portrait of my mom.'}>
+				<div className="type-work"><img src={type1} alt="Type study one" /><img src={type2} alt="Type study two" /><img src={type3} alt="Type study three" /><img className="brooke" src={brooke} alt="Brooke type study" /></div>
+			</Section>
+			<Section id="sputnik" number="05" title="Sputnik Branding" blurb={'A passion project exploring branding and packaging design.\n\nSputnik Tennis Balls came from two brief moments of inspiration. I was first inspired by the bold, eye-catching design of vintage tennis ball containers, which feature contrasting typography, fun graphics, and a playfulness that has been lost in modern sports marketing. Completely separately, I was struck by the shape of the Soviet Union’s Sputnik 1 satellite. I used an image of it as a reference when designing the flying tennis ball on the can (the tennis ball serves the satellite body while the white lines serve as the satellite legs), which I think brings the whole concept together.\n\nI created the vector art in Adobe Illustrator, and I created the mockup in Womp 3D and Adobe Photoshop. '}>
 				<div className="sputnik-work">
 					<img className="sputnik-wide" src={canWord} alt="Sputnik word can mockup" />
 					<img src={canMockup} alt="Sputnik can mockup" /><img className="hi" src={packaging} alt="Sputnik packaging flat lay" />
 				</div>
-			</Section>
-			<Section id="type-studies" number="04" title="Type Studies" blurb={'Various project with typography.\n\nIn my spare time, I like playing around with type by making posters. The first three are brief type studies, and the “Brooke” poser is a typographic portrait of my mom.'}>
-				<div className="type-work"><img src={type1} alt="Type study one" /><img src={type2} alt="Type study two" /><img src={type3} alt="Type study three" /><img className="brooke" src={brooke} alt="Brooke type study" /></div>
 			</Section>
 		</main>
 	)
